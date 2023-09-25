4fc17fc133b2dd4fb7791c2d682994a0894ce112
Jan Stanisław Nowacki <j.nowacki53@gmail.com>
2023-09-25 12:55:08 +0200

    added a view

--- app/routes/web.php
+++ app/routes/web.php
@@ -22,6 +22,15 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('info',function() {
+    return view('info',[
+        'firstName' => 'Janusz',
+        'lastName' => 'Kowalski',
+        'city' => 'Łódź'
+    ]);
+});
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');

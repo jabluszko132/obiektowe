1705728841a86785075d3db657e4f8a3763d1c74
Jan Stanislaw Nowacki <j.nowacki53@gmail.com>
2023-10-23 09:34:38 +0200

    end of lesson

--- 23_10/app/Http/Controllers/ShowController.php
+++ 23_10/app/Http/Controllers/ShowController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class ShowController extends Controller
 {
-    //
+    public function showView() : View {
+        $users = [
+            'firstName' => 'Janusz',
+            'lastName' => 'Nowak',
+            'city' => 'Poznań',
+            'hobby' => [
+                'siatkowka',
+                'skoki narciarskie',
+                'żużel',
+                'piłka ręczna'
+            ]
+        ];
+
+        return view('users',$users);
+    }
+    public function showArray() : View {
+        $users = [
+            'firstName' => 'Janusz',
+            'lastName' => 'Nowak',
+            'city' => 'Poznań',
+            'hobby' => [
+                'siatkowka',
+                'skoki narciarskie',
+                'żużel',
+                'piłka ręczna'
+            ]
+        ];
+
+        return view('usersarray',['users' => $users]);
+    }
 }
--- 23_10/routes/web.php
+++ 23_10/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ShowController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +22,11 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('showusers', [ShowController::class, 'showView']);
+Route::get('showarray', [ShowController::class, 'showArray']);
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');

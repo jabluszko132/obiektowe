056dc5149fc754d1a043b679cc3b94e27e9e0270
Jan Stanislaw Nowacki <j.nowacki53@gmail.com>
2023-10-23 12:52:03 +0200

    added new form

---
+++ 23_10_2023/resources/views/forms/user_form.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Dane użytkownika</h3>
+    <form action="" method="get">
+        <input type="text" name="" id="">
+    </form>
+</body>
+</html>

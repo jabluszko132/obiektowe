ee01e62c5f52028d7274db238da2883dee6f04dd
Jan Stanislaw Nowacki <j.nowacki53@gmail.com>
2023-10-30 09:34:14 +0100

    end of lesson

---
+++ 30_10/app/Http/Controllers/UserFormController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserFormController extends Controller
+{
+    public function showUser(Request $req){
+        $req->validate([
+            "firstName"=> "required | min:2 | max:15",
+            "lastName"=> "required | min:2 | max:30",
+            "email"=> "required | min:5 | max:15 | email",
+        ]);
+        // return $req->input();
+
+        $user = [
+            'firstName'=> $req->input('firstName'),
+            'lastName'=> $req->input('lastName'),
+            'email'=> $req->input('email'),
+        ];
+        return view('user', ['user'=> $user]);
+    }
+}
---
+++ 30_10/resources/views/forms/user_form.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Formularz użytkownika</title>
+</head>
+<body>
+    <h3>Dane użytkownika</h3>
+    <form action="UserFormController" method="get">
+        <input type="text" name="firstName" placeholder="Podaj imię" autofocus><br><br>
+        <input type="text" name="lastName" placeholder="Podaj nazwisko"><br><br>
+        <input type="email" name="email" placeholder="Podaj email"><br><br>
+        <input type="submit" value="Zatwierdź dane">
+
+    </form>
+</body>
+</html>
